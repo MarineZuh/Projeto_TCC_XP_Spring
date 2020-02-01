39d457252fb772d843b657b369755546e06cd687
Kerlyson <kerlyson_sk@yahoo.com.br>
2020-02-01 18:50:54 -0300

    projeto inicial

---
+++ src/test/java/com/xp/tcc/tcc/TccApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xp.tcc.tcc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TccApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
