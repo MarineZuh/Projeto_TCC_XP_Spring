b4ff2d2565fcbd6f41482c5d9545f24eaff8fbdf
KELLY SOUSA <KELLY.SOUSA@ACAD.IFMA.EDU.BR>
2020-02-02 12:08:03 -0300

    Criação da tabela aluno, contendo nome, id, curso, email

---
+++ src/main/resources/db/migration/V02_criação_tabela_aluno.sql
@@ -0,0 +1,14 @@
+use projeto;
+
+create table aluno (
+	nome varchar (40),
+    id int auto_increment,
+    curso varchar (40),
+    email varchar (40),
+    primary key (id)
+);
+    insert into aluno (nome, curso, email) values ('Maria Joaquina Oliveira', 'Sistemas de informação', 'mjoa@hotmail.com');
+    insert into aluno (nome, curso, email) values ('Maria Cecilia', 'Sistemas de informação', 'ceciliaa@edu.acad.com');
+    insert into aluno (nome, curso, email) values ('José Bento', 'Sistemas de informação', 'joeBento@acad.edu.com');
+    
+    
